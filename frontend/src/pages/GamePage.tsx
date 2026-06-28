9093cca89038d2fb04e4b35711e4dfed2077f624
OhadGriner <ohad.griner@gmail.com>
2026-06-28 18:04:44 +0200

    Initial frontend

---
+++ frontend/src/pages/GamePage.tsx
@@ -0,0 +1,7 @@
+export default function GamePage() {
+  return (
+    <div className="flex items-center justify-center min-h-dvh">
+      <p className="text-2xl font-bold">מחרוזות 🧩</p>
+    </div>
+  )
+}
